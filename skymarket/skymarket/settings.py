adc890747f6dafc2554558d831cf7297ab300e31
User <dossovmarlen85@gmail.com>
2023-06-08 21:05:49 +0300

    added Pagination

--- skymarket/skymarket/settings.py
+++ skymarket/skymarket/settings.py
@@ -87,10 +87,12 @@ WSGI_APPLICATION = "skymarket.wsgi.application"
 
 
 REST_FRAMEWORK = {
-    "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
-    ),
-    # 'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 }
 
 DJOSER = {
--- skymarket/users/urls.py
+++ skymarket/users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
 from djoser.views import UserViewSet
 from rest_framework.routers import SimpleRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 users_router = SimpleRouter()
 
@@ -10,4 +11,7 @@ users_router.register("users", UserViewSet, basename="users")
 
 urlpatterns = [
     path("", include(users_router.urls)),
+    path("token/", TokenObtainPairView.as_view()),
+    path("refresh/", TokenRefreshView.as_view()),
+
 ]
